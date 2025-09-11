84f33aad13b19f1b7ad434f54e24569220f99d7a
Dongwoo Yang <ckck803@gmail.com>
2025-09-11 16:46:34 +0900

    패딩 수정

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex flex-col mx-auto py-8 items-center justify-center">
     <div
-      class="px-12 lg:px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-x-14 max-w-7xl w-full justify-items-center"
+      class="px-4 lg:px-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-x-14 max-w-7xl w-full justify-items-center"
     >
       <!-- 로딩 중일 때 스켈레톤 UI 표시 -->
       <template v-if="appStore.isLoading">
